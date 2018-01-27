719dbbbf82fc0632db3570c5efed88f88f12707a
Arvind Raghavan <raghavan@wireless-10-148-91-148.public.utexas.edu>
2018-01-27 17:05:34 -0600

    Added Event class

--- app/src/main/java/kushangupta/volunteerapp/MainActivity.java
+++ app/src/main/java/kushangupta/volunteerapp/MainActivity.java
@@ -1,7 +1,9 @@
 package kushangupta.volunteerapp;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.location.Location;
 import android.util.Log;
 
 import com.google.firebase.database.DatabaseReference;
@@ -15,16 +17,31 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        // null
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // Write a message to the database
         myRef = FirebaseDatabase.getInstance().getReference();
         myRef.child("User").setValue("bob");
-
-
-
     }
 
 
+}
+
+class Event {
+
+    private String title;
+    private String description;
+    private int numPeople;
+    private int numPeopleRequired;
+
+    String loc;
+    private Image img;
+
+    public Event(String title, String description, int numPeople, int numPeopleRequired, String loc) {
+        this.title = title;
+        this.description = description;
+        this.numPeople = numPeople;
+        this.numPeopleRequired = numPeopleRequired;
+        this.loc = loc;
+    }
 }
