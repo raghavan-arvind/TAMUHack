15e36e96803ff68f1a2e2c5c1899bf985fb75a73
brett.medina <brett.medina@utexas.edu>
2018-01-27 18:00:59 -0600

    Added login, main, signup, volunteer

--- app/src/main/java/kushangupta/volunteerapp/MainActivity.java
+++ app/src/main/java/kushangupta/volunteerapp/MainActivity.java
@@ -1,7 +1,11 @@
 package kushangupta.volunteerapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 import com.google.firebase.database.DatabaseReference;
@@ -17,7 +21,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // for testing
+        Button orgLoginBtn = (Button) findViewById(R.id.orgLoginBtn);
+        Button volLoginBtn = (Button) findViewById(R.id.volunteerLoginBtn);
+        TextView alreadyTv = (TextView) findViewById(R.id.alreadyTv);
+
+        /* user clicked organization */
+        orgLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, OrgSignup.class);
+                startActivity(intent);
+            }
+        });
+
+        /* user clicked volunteer */
+        volLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, VolunteerSignup.class);
+                startActivity(intent);
+            }
+        });
+
+        /* user clicked already account */
+        alreadyTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
