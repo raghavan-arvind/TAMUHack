4d892f1fbd32c0cc450a1c0975f4c296780ec4c5
Kushangupta <kushangupta@gmail.com>
2018-01-28 02:19:51 -0600

    added class back

---
+++ app/src/main/java/kushangupta/volunteerapp/Event_Add.java
@@ -0,0 +1,18 @@
+package kushangupta.volunteerapp;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class Event_Add extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_add);
+    }
+}
+
