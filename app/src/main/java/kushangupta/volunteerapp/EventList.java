4c207dc75e27b81f3f0b317e78cd8db63a3c5191
brett.medina <brett.medina@utexas.edu>
2018-01-28 04:41:05 -0600

    EVERYTHING WORKING.

--- app/src/main/java/kushangupta/volunteerapp/EventList.java
+++ app/src/main/java/kushangupta/volunteerapp/EventList.java
@@ -5,22 +5,33 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class EventList extends AppCompatActivity {
+import org.w3c.dom.Text;
 
+import java.util.ArrayList;
+
+public class EventList extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    ListView lst;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_list);
 
-        FloatingActionButton addEventBtn = (FloatingActionButton) findViewById(R.id.addEventBtn);
         ListAdapter listAdapter = new ArrayAdapter<Event>(this, android.R.layout.simple_list_item_1, BackEnd.getEvents());
         ListView listView = (ListView) findViewById(R.id.eventsListView);
         listView.setAdapter(listAdapter);
+
+
+        FloatingActionButton addEventBtn = (FloatingActionButton) findViewById(R.id.addEventBtn);
 
         addEventBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,4 +41,10 @@ public class EventList extends AppCompatActivity {
             }
         });
     }
-}
+
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            TextView tv = (TextView) view;
+            Toast.makeText(this, "You click on" + tv.getText() + position, Toast.LENGTH_SHORT).show();
+        }
+    }
+
