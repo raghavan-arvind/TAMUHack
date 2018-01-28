9e429f2ecf76cd1fd063d11b2a3633c96b5aa0a7
Arvind Raghavan <raghavan@wireless-10-148-91-148.public.utexas.edu>
2018-01-28 07:23:40 -0600

    fix date?

--- app/src/main/java/kushangupta/volunteerapp/Event_Add.java
+++ app/src/main/java/kushangupta/volunteerapp/Event_Add.java
@@ -1,6 +1,7 @@
 package kushangupta.volunteerapp;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 
 
@@ -12,12 +13,15 @@ import android.widget.CalendarView;
 import android.widget.EditText;
 import android.widget.NumberPicker;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 
 public class Event_Add extends AppCompatActivity {
+
+    int year = 0; int month = 0; int day = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +34,14 @@ public class Event_Add extends AppCompatActivity {
         final NumberPicker numPeople = (NumberPicker) findViewById(R.id.numberPicker);
         final CalendarView eventDate = (CalendarView) findViewById(R.id.eventDate);
 
+        eventDate.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
+                year = i; month = i1+1; day = i2;
+            }
+        });
+        final TimePicker time = (TimePicker) findViewById(R.id.eventDate);
+
         Button eventCreateBtn = (Button) findViewById(R.id.eventCreateBtn);
 
         eventCreateBtn.setOnClickListener(new View.OnClickListener() {
@@ -38,14 +50,12 @@ public class Event_Add extends AppCompatActivity {
                 String name = eventName.getText().toString();
                 String desc = eventDescript.getText().toString();
                 String address = eventAddress.getText().toString();
+                String t = (time.getHour() > 12 ? time.getHour()-12: time.getHour()) + " " + time.getMinute() + (time.getHour() > 12 ? "PM" : "AM");
                 int num = numPeople.getValue();
 
                 // converting date to readable
-                long date = eventDate.getDate();
-                SimpleDateFormat sdf = new SimpleDateFormat("MMM dd,yyyy HH:mm");
-                Date resultdate = new Date(date);
 
-                String dateString = sdf.format(resultdate);
+                String dateString = String.format("%d/%d/%d ", month, day, year) + t;
 
                 BackEnd.addEvent(name, desc, 0, num, address, dateString);
                 //onSelectedDayChange(eventDate, );
