95a14ea426de315e79183964e4e8a35df5927421
Kushangupta <kushangupta@gmail.com>
2018-01-28 04:22:49 -0600

    ArrayAdapter working and list scroll implemented, and event adding

--- app/src/main/java/kushangupta/volunteerapp/EventList.java
+++ app/src/main/java/kushangupta/volunteerapp/EventList.java
@@ -5,6 +5,9 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 public class EventList extends AppCompatActivity {
 
@@ -15,6 +18,9 @@ public class EventList extends AppCompatActivity {
         setContentView(R.layout.activity_event_list);
 
         FloatingActionButton addEventBtn = (FloatingActionButton) findViewById(R.id.addEventBtn);
+        ListAdapter listAdapter = new ArrayAdapter<Event>(this, android.R.layout.simple_list_item_1, BackEnd.getEvents());
+        ListView listView = (ListView) findViewById(R.id.eventsListView);
+        listView.setAdapter(listAdapter);
 
         addEventBtn.setOnClickListener(new View.OnClickListener() {
             @Override
