d4f05e63446cae8519447c59407df273228083f8
Kushangupta <kushangupta@gmail.com>
2018-01-28 10:18:12 -0600

    finished a lot

--- app/src/main/java/kushangupta/volunteerapp/EventSignup.java
+++ app/src/main/java/kushangupta/volunteerapp/EventSignup.java
@@ -1,8 +1,14 @@
 package kushangupta.volunteerapp;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class EventSignup extends AppCompatActivity {
@@ -11,6 +17,37 @@ public class EventSignup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_signup);
+        String[] images = new String[7];
+        for(int i = 1; i <= 7; i++){
+            images[i - 1] = "@drawable/image" + i;
+        }
+        int randInt = (int) (Math.random() * 7);
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+
+        switch (randInt) {
+            case 0:
+                imageView.setImageResource(R.drawable.image1);
+                break;
+            case 1:
+                imageView.setImageResource(R.drawable.image2);
+                break;
+            case 2:
+                imageView.setImageResource(R.drawable.image3);
+                break;
+            case 3:
+                imageView.setImageResource(R.drawable.image4);
+                break;
+            case 4:
+                imageView.setImageResource(R.drawable.image5);
+                break;
+            case 5:
+                imageView.setImageResource(R.drawable.image6);
+                break;
+            case 6:
+                imageView.setImageResource(R.drawable.image7);
+                break;
+
+        }
         int pos = getIntent().getIntExtra("Position", 0);
         Event e = EventList.myEvents.get(pos);
 
@@ -21,6 +58,15 @@ public class EventSignup extends AppCompatActivity {
         TextView numPeople = (TextView) findViewById(R.id.numPeopleTv);
 
         Button regBtn = (Button) findViewById(R.id.regBtn);
+                /* user clicked organization */
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(EventSignup.this, EventList.class);
+                intent.putExtra("SOURCE", "org_signup");
+                startActivity(intent);
+            }
+        });
 
         titleTvTv.setText(e.getTitle());
 
@@ -30,6 +76,14 @@ public class EventSignup extends AppCompatActivity {
         numPeople.setText(Integer.toString(e.numPeopleRequired));
 
 
+
     }
+
+    public static String addImageToGallery(final String filePath, final Context context) {
+
+        return filePath;
+    }
+
+
 
 }
